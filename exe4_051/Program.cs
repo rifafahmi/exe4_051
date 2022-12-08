1ec0b48ac084aa1ec8049b3cd9905503faf4429c
Rifa <rifafahim21.com@gmial.com>
2022-12-08 16:17:12 +0700

    Add project files.

---
+++ exe4_051/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace exe4_051
+{   
+    class Node
+    {
+        public int info;
+        public Node next;
+        public Node(int 1, Node n)
+        {
+            info = r;
+            next = a;
+        }
+    }
+    class stacks
+    {
+        Node top;
+
+        public stacks()
+        {
+            top = null;
+        }
+        bool empty()
+        {
+
+        }
+    }
+}
